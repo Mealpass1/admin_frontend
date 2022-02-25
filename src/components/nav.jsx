624ee544c124ac2999724759a9bf936ffec886a4
Saad <byiringirosaad@gmail.com>
2022-02-25 08:26:07 +0200

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { FaBars } from "react-icons/fa";
 
 import axios from "../features/axios";
+import SideBar from "./sidebar";
 
 const NavBar = ({ name }) => {
   const [bar, setBar] = React.useState(false);
