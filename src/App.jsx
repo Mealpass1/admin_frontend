1e36fd403cf3a8b314deb79467bae600234df68d
Saad <byiringirosaad@gmail.com>
2022-02-28 06:37:02 +0200

    added width warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //packages
-import React from "react";
+import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,20 +10,31 @@ import ForgotPassword from "./pages/forgotpassword";
 import Diners from "./pages/diners";
 import Restaurants from "./pages/restaurants";
 import Transactions from "./pages/transactions";
+import Width from "./components/width";
 
 function App() {
-  return (
-    <Router>
-      <ToastContainer />
-      <Routes>
-        <Route path="/" exact element={<Login />} />
-        <Route path="/forgotpassword" exact element={<ForgotPassword />} />
-        <Route path="/diners" exact element={<Diners />} />
-        <Route path="/restaurants" exact element={<Restaurants />} />
-        <Route path="/transactions" exact element={<Transactions />} />
-      </Routes>
-    </Router>
-  );
+  const [width, setWidth] = React.useState("");
+
+  React.useEffect(() => {
+    setWidth(window.innerWidth);
+  });
+
+  if (width > 416) {
+    return <Width />;
+  } else {
+    return (
+      <Router>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+          <Route path="/forgotpassword" exact element={<ForgotPassword />} />
+          <Route path="/diners" exact element={<Diners />} />
+          <Route path="/restaurants" exact element={<Restaurants />} />
+          <Route path="/transactions" exact element={<Transactions />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
