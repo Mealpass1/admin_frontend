ee719fb923b81b36cc95af464cc259a35ed374e9
Saad <byiringirosaad@gmail.com>
2022-05-26 08:27:57 +0200

    update backend url

--- src/features/axios.js
+++ src/features/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "http://localhost:8080",
-  baseURL: "https://meals-back.herokuapp.com",
+  baseURL: "https://mealbackend.herokuapp.com/",
 });
